b5d5df5657a65afc1890027de00f36284c135e9f
Illia Shatokhin <resonane.2106@gmail.com>
2023-08-04 22:12:15 +0300

    add loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,29 +4,34 @@ import { ContactsList } from './contacts-list/ContactsList';
 import { PhonebookForm } from './phonebook-form/PhonebookForm';
 import { LabelInput } from './label-input/LabelInput';
 import { filterChange } from 'redux/filterSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchContactsThunk } from 'redux/operations';
+import { selectIsLoading } from 'redux/selectors';
+import LoadingOverlay from 'react-loading-overlay-ts';
 
 export const App = () => {
+  const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchContactsThunk());
   }, [dispatch]);
   return (
-    <div className={css.container}>
-      <HeadText title="Phonebook" />
-      <PhonebookForm />
-      <HeadText title="Contacts" />
-      <LabelInput
-        handleInputChange={e => {
-          dispatch(filterChange(e.target.value));
-        }}
-        type="text"
-        name="filter"
-        labelName="Find contacts by name"
-      />
-      <ContactsList />
-    </div>
+    <LoadingOverlay active={isLoading} spinner text="Loading your content...">
+      <div className={css.container}>
+        <HeadText title="Phonebook" />
+        <PhonebookForm />
+        <HeadText title="Contacts" />
+        <LabelInput
+          handleInputChange={e => {
+            dispatch(filterChange(e.target.value));
+          }}
+          type="text"
+          name="filter"
+          labelName="Find contacts by name"
+        />
+        <ContactsList />
+      </div>
+    </LoadingOverlay>
   );
 };
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,8 +1,8 @@
 import { createSelector } from '@reduxjs/toolkit';
 
 export const selectContacts = state => state.contacts.contacts;
-export const selectIsLoading = state => state.isLoading.contacts;
-export const selectError = state => state.error.contacts;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter.filter;
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFilter],
